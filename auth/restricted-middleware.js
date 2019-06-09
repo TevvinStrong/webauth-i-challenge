c01e87f974dac4b92ab1b831bb9a8fd016dc5281
Tevvin <Tevvin7395@icloud.com>
2019-06-08 21:40:03 -0400

    mvp pt2 complete

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -4,24 +4,12 @@ const Users = require('../data/helpers/usersModel.js');
 
 
 function restricted(req, res, next) {
-    const { username, password } = req.headers;
+    //const { username, password } = req.headers;
 
-    if (username && password) {
-        Users.findBy({ username })
-            .first()
-            .then(user => {
-                if (user && bcrypt.compareSync(password, user.password)) {
-                    //res.status(200).json({ message: `Welcome ${user.username}!` });
-                    next();
-                } else {
-                    res.status(401).json({ message: "You shall not pass!" });
-                }
-            })
-            .catch(error => {
-                res.status(500).json({ message: "We ran into an error retreving the specified request.", error });
-            });
+    if (req.session && req.session.user) {
+        next();
     } else {
-        res.status(400).json({ message: "Pleae provide credentials." });
+        res.status(401).json({ message: "You shall not pass!" });
     }
 }
 
