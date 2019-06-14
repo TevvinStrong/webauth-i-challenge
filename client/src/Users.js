7d72b9e1cd427009e8b22fa2ca35a1bcb601bb32
Tevvin <Tevvin7395@icloud.com>
2019-06-14 01:11:19 -0400

    Components created

---
+++ client/src/Users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Users extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Users</h3>
+            </div>
+        )
+    }
+}
+
+export default Users;
