5a94cad550ce392b1a822b5d41554e71d45dac1c
Tevvin <Tevvin7395@icloud.com>
2019-06-14 16:31:24 -0400

    Login component complete, and functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Route, NavLink } from 'react-router-dom';
+import Login from './Login.js';
+import Signup from './Signup.js';
+import Users from './Users.js';
 
 function App() {
   return (
-  <div>
-    Hello World!  
-  </div>
+    <div>
+      <h1>Welcome!</h1>
+
+      <ul>
+        <li><NavLink to="/login">Login</NavLink></li>
+        <li><NavLink to="/signup">Signup</NavLink></li>
+        <li><NavLink to="/users">Users</NavLink></li>
+      </ul>
+
+      <main>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/users" component={Users} />
+      </main>
+    </div>
   );
 }
 
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import axios from 'axios';
 
 class Login extends React.Component {
+    state = {
+        username: '',
+        password: '',
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(this.state);
+
+        try {
+            const endpoint = 'http://localhost:5000/api/login';
+            const result = await axios.post(endpoint, {
+                username: this.state.username,
+                password: this.state.password,
+            })
+
+            console.log(result);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    }
+
     render() {
         return (
             <div>
                 <h3>Login</h3>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="username" placeholder="Username" onChange=
+                        {this.handleChange} value={this.state.username} />
+                    <input type="password" name="password" placeholder="Password" onChange=
+                        {this.handleChange} value={this.state.password} />
+                    <button typer="submit">Login</button>
+                </form>
             </div>
         )
     }
